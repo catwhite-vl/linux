395aeb63e766c6342b9026050d7a716b7c087203
catwhite-vl <136222036+catwhite-vl@users.noreply.github.com>
2023-07-23 00:38:06 +1000

    Create InstallArchLinux_uefi_2.sh

---
+++ InstallArchLinux_uefi_2.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asia/Vladivostok /etc/localtime
+hwclock --systohc
+
+sed -i '398s/.//' /etc/locale.gen
+sed -i '171s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
+echo "FONT=cyr-sun16" >> /etc/vconsole.conf
+echo "KEYMAP=ru" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+#pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -Sy grub efibootmgr nano sudo dhcpcd  base-devel  openssh os-prober ntfs-3g terminus-font mc neofetch bashtop open-vm-tools usbutils
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB  --removable
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#systemctl enable NetworkManager
+#systemctl enable bluetooth
+#systemctl enable cups.service
+systemctl enable sshd
+systemctl enable dhcpcd
+systemctl enable vmtoolsd.service
+#systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#systemctl enable reflector.timer
+#systemctl enable fstrim.timer
+#systemctl enable libvirtd
+#systemctl enable firewalld
+#systemctl enable acpid
+
+echo root:password | chpasswd
+#useradd -m sergey
+#echo sergey:password | chpasswd
+#usermod -aG libvirt ermanno
+useradd -m -g users -G wheel -s /bin/bash sergey
+
+echo "sergey ALL=(ALL) ALL" >> /etc/sudoers.d/00_sergey
+
+
+printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m"
+
+
+
